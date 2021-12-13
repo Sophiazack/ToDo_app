720d6e427709a1a99aa5b2d8c2ca45915404b8e1
sophiazack <sophiazakari717@gmail.com>
2021-12-13 17:11:31 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'home_view.dart';
+
+void main() {
+  runApp(DevicePreview(
+    enabled: !kReleaseMode,
+    builder:(context)=> const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      title: 'Awesome Todo',
+      theme: ThemeData(
+          primaryColor: const Color.fromRGBO(37, 43, 103, 1),
+          appBarTheme: const AppBarTheme(
+              backgroundColor: Color.fromRGBO(37, 43, 103, 1),
+              elevation: 0)
+              ),
+      home:const HomeView(),
+    );
+  }
+}
